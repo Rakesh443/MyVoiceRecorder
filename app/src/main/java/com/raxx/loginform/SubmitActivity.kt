199431b4250f6960c9a97c4626bab6524866e37a
Rakesh443 <rk20035@gmail.com>
2020-08-23 18:06:09 +0530

    Added Form

---
+++ app/src/main/java/com/raxx/loginform/SubmitActivity.kt
@@ -0,0 +1,21 @@
+package com.raxx.loginform
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
+
+class SubmitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit)
+    }
+
+    fun datepick(view: View) {
+
+       // var date1 =
+
+        
+    }
+}
