aadf67885cfb99afa12ddedc34a9a61d25c9051f
Rakesh443 <rk20035@gmail.com>
2020-09-15 20:23:45 +0530

    Login BackButton Updated

--- app/src/main/java/com/raxx/loginform/MainActivity.kt
+++ app/src/main/java/com/raxx/loginform/MainActivity.kt
@@ -6,14 +6,18 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.view.View
 import android.view.Window
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.otp.*
 
 class MainActivity : AppCompatActivity() {
     var db = UserDataBaseHandler(this)
+    private val TIME_INTERVAL = 2000
+    private var mBackPressed: Long = 0
     //var sharedPreferences:SharedPreferences=getSharedPreferences("Login",Context.MODE_PRIVATE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,16 +28,60 @@ class MainActivity : AppCompatActivity() {
         signupId.setOnClickListener {
             val  intent= Intent(this, SignupActivity::class.java)
             startActivity(intent)
-//            var dialog= Dialog(this)
-//            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-//            dialog.setCancelable(true)
-//            dialog.setContentView(R.layout.otp)
-//            val btn = dialog.findViewById(R.id.otpButton) as Button
-//            btn.setOnClickListener { dialog.dismiss() }
-//            dialog.show()
+
+
+        }
+
+        forgotId.setOnClickListener {
+            Toast.makeText(applicationContext, "forgot shdgfsfhvhf", Toast.LENGTH_SHORT).show()
+            if(nameId.text.toString().length<0) {
+                nameId.setError("Enter Phone Number")
+            }
+            var list = db.getallData()
+            var j=0
+            for(i in list){
+
+                if(i.name.toString()==nameId.text.toString())
+                    j++
+            }
+            if(j>0){
+                val numb:String="9700349127"
+                val mesg:String="4565"
+                val sms: SmsManager =SmsManager.getDefault()
+                sms.sendTextMessage(numb,null,mesg,null,null)
+
+                var intent = Intent(Intent.ACTION_VIEW);
+                verify(mesg)
+            }
 
         }
     }
+
+    override fun onBackPressed() {
+        if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+           finish()
+            return
+        } else {
+            Toast.makeText(baseContext, "Tap back button in order to exit", Toast.LENGTH_SHORT)
+                .show()
+        }
+        mBackPressed = System.currentTimeMillis()
+    }
+
+    private fun verify(mesg: String) {
+        var dialog= Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.otp)
+        val btn = dialog.findViewById(R.id.otpButton) as Button
+        btn.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+        if(otp.text.toString()==mesg){
+            val  intent1= Intent(this, SubmitActivity::class.java)
+        }
+
+    }
+
 
     fun login(view: View) {
         val name:String=nameId.text.toString()
